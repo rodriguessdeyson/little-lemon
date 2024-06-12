1a4706f0c0ee5d5683aa696c1f7e1a12169d25ce
Deyson Alves Rodrigues <rodriguessdeyson@gmail.com>
2024-06-11 21:19:13 -0300

    "Set up the semantic HTML"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import Header from './components/Header'
+import Navbar from './components/Navigation/Nav'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+    </>
   );
 }
 
