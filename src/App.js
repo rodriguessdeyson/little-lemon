5a565ead5095e984791319876612ab4debf37800
Deyson Alves Rodrigues <rodriguessdeyson@gmail.com>
2024-06-17 00:10:43 -0300

    Set up react route and links

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import Header from './components/Header'
 import Navbar from './components/Navigation/Nav'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import { Routes, Route } from 'react-router-dom'
 
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <Navbar></Navbar>
-    </>
-  );
+	return (
+		<>
+			<Navbar></Navbar>
+			<Routes>
+				{/* <Route path="/" element={<Homepage />} /> */}
+				{/* <Route path="/about" element={<About />} /> */}
+				{/* <Route path="/reservations" element={<Reservation />} /> */}
+				{/* <Route path="/order" element={<Order />} /> */}
+				{/* <Route path="/login" element={<Login />} /> */}
+				{/* <Route path="/confirmation" element={<Confirmation />} /> */}
+			</Routes>
+		</>
+	);
 }
 
 export default App;
