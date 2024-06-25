fe86274dca454635bcfb476bd027950a718c33e9
Deyson Alves Rodrigues <rodriguessdeyson@gmail.com>
2024-06-25 15:39:18 -0300

    Fix the test and the pipeline

--- src/app/Reservation/tests/reservations.test.jsx
+++ src/app/Reservation/tests/reservations.test.jsx
@@ -23,4 +23,30 @@ describe("Reservations page", () => {
 			expect(timeOption.value).toMatch(timeFormat)
 		);
 	});
+
+	test("should update available reservation time options when changing reservation date", async () => {
+		render(
+			<MemoryRouter>
+				<Reservations />
+			</MemoryRouter>
+		);
+
+		const date = new Date();
+		date.setDate(date.getDate() + 2);
+		const reservationDate = date.toISOString().split('T')[0];
+		const initialTimeOptions = await screen.findAllByTestId("reservation-time-option");
+		const dateInput = screen.getByLabelText(/Date/);
+		fireEvent.change(dateInput, { target: { value: reservationDate } });
+		fireEvent.blur(dateInput);
+		const updatedTimeOptions =  await screen.findAllByTestId("reservation-time-option");
+
+		expect(dateInput).toHaveValue(reservationDate);
+		initialTimeOptions.forEach((timeOption) =>
+			expect(timeOption.value).toMatch(timeFormat)
+		);
+		updatedTimeOptions.forEach((timeOption) =>
+			expect(timeOption.value).toMatch(timeFormat)
+		);
+		expect(initialTimeOptions.length).not.toEqual(updatedTimeOptions.length);
+	});
 });
