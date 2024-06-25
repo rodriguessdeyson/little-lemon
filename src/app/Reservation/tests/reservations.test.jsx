8a0eea3094ad58f7e9a9a510c558120cf93db744
Deyson Alves Rodrigues <rodriguessdeyson@gmail.com>
2024-06-25 12:52:14 -0300

    Fix pipeline

--- src/app/Reservation/tests/reservations.test.jsx
+++ src/app/Reservation/tests/reservations.test.jsx
@@ -1,8 +1,14 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Reservations from "../../Reservation/pages/ReservationTable";
+window.scrollTo = jest.fn();
 
 describe("Reservations page", () => {
+
+	afterAll(() => {
+		jest.clearAllMocks();
+	});
+
 	const timeFormat = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
 
 	test("should have one or more available reservation time options", async () => {
